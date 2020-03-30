abb0837fecd0fa28d64450a72878b0bf314a856f
doltonfernandes <dolton.fernandes@gmail.com>
2020-03-30 20:58:38 +0530

    .

--- assets/js/corona.js
+++ assets/js/corona.js
@@ -6,36 +6,6 @@ var critical = 0;
 var recovered = 0;
 var total = 1;
 
-function make_data() {
-
-	var data = null;
-
-	var xhr = new XMLHttpRequest();
-	xhr.withCredentials = true;
-
-	xhr.addEventListener("readystatechange", function () {
-		if (this.readyState === this.DONE) {
-		    var tmp = JSON.parse(this.responseText)["response"];
-			for (var key in tmp) {
-			    obj.push(tmp[key]);
-			}
-    		document.getElementById('thr').innerHTML += '<th><h1>Sr No.</h1></th><th onclick="sort_country()"><h1 id="country">Country↓</h1></th>';
-		    for(var key in obj[0]["cases"])
-		    {
-	    		document.getElementById('thr').innerHTML += '<th onclick="sort_' + key +  '()"><h1 id="' + key + '">' + key + '↓</h1></th>';
-		    }
-		    sort_total();
-		    make_table();
-		}
-	});
-
-	xhr.open("GET", "https://covid-193.p.rapidapi.com/statistics");
-	xhr.setRequestHeader("x-rapidapi-host", "covid-193.p.rapidapi.com");
-	xhr.setRequestHeader("x-rapidapi-key", "604a6ae58cmsh1d0fd5d11558425p1ef0f9jsn66c6876e2c56");
-
-	xhr.send(data);
-}
-
 function make_table() {
 	document.getElementById("tbd").innerHTML = "";
 	var cnt = 0;
@@ -56,7 +26,7 @@ function sort_country() {
 	obj.sort(function(a, b) {
 		return (a["country"] > b["country"])^country;
 	})
-	document.getElementById('country').textContent = 'country' + (country ? '↓':'↑');
+	document.getElementById('country').textContent = 'COUNTRY' + (country ? '↓':'↑');
 	country = !country;
 	document.getElementById("country");
 	make_table();
@@ -67,7 +37,7 @@ function sort_new() {
 	obj.sort(function(a, b) {
 		return (a["cases"]["new"] > b["cases"]["new"])^neww;
 	})
-	document.getElementById('new').textContent = 'new' + (neww ? '↓':'↑');
+	document.getElementById('new').textContent = 'NEW' + (neww ? '↓':'↑');
 	neww = !neww;
 	make_table();
 }
@@ -76,7 +46,7 @@ function sort_active() {
 	obj.sort(function(a, b) {
 		return (a["cases"]["active"] > b["cases"]["active"])^active;
 	})
-	document.getElementById('active').textContent = 'active' + (active ? '↓':'↑');
+	document.getElementById('active').textContent = 'ACTIVE' + (active ? '↓':'↑');
 	active = !active;
 	make_table();
 }
@@ -85,7 +55,7 @@ function sort_critical() {
 	obj.sort(function(a, b) {
 		return (a["cases"]["critical"] > b["cases"]["critical"])^critical;
 	})
-	document.getElementById('critical').textContent = 'critical' + (critical ? '↓':'↑');
+	document.getElementById('critical').textContent = 'CRITICAL' + (critical ? '↓':'↑');
 	critical = !critical;
 	make_table();
 }
@@ -94,7 +64,7 @@ function sort_recovered() {
 	obj.sort(function(a, b) {
 		return (a["cases"]["recovered"] > b["cases"]["recovered"])^recovered;
 	})
-	document.getElementById('recovered').textContent = 'recovered' + (recovered ? '↓':'↑');
+	document.getElementById('recovered').textContent = 'RECOVERED' + (recovered ? '↓':'↑');
 	recovered = !recovered;
 	make_table();
 }
@@ -103,7 +73,37 @@ function sort_total() {
 	obj.sort(function(a, b) {
 		return (a["cases"]["total"] > b["cases"]["total"])^total;
 	})
-	document.getElementById('total').textContent = 'total' + (total ? '↓':'↑');
+	document.getElementById('total').textContent = 'TOTAL' + (total ? '↓':'↑');
 	total = !total;
 	make_table();
+}
+
+function make_data() {
+
+	var data = null;
+
+	var xhr = new XMLHttpRequest();
+	xhr.withCredentials = true;
+
+	xhr.addEventListener("readystatechange", function () {
+		if (this.readyState === this.DONE) {
+		    var tmp = JSON.parse(this.responseText)["response"];
+			for (var key in tmp) {
+			    obj.push(tmp[key]);
+			}
+    		document.getElementById('thr').innerHTML += '<th><h1>Sr No.</h1></th><th onclick="sort_country()"><h1 id="country">COUNTRY↓</h1></th>';
+		    for(var key in obj[0]["cases"])
+		    {
+	    		document.getElementById('thr').innerHTML += '<th onclick="sort_' + key +  '()"><h1 id="' + key + '">' + key.toUpperCase() + '↓</h1></th>';
+		    }
+		    sort_total();
+		    make_table();
+		}
+	});
+
+	xhr.open("GET", "https://covid-193.p.rapidapi.com/statistics");
+	xhr.setRequestHeader("x-rapidapi-host", "covid-193.p.rapidapi.com");
+	xhr.setRequestHeader("x-rapidapi-key", "604a6ae58cmsh1d0fd5d11558425p1ef0f9jsn66c6876e2c56");
+
+	xhr.send(data);
 }
